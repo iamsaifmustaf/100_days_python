d4c59cc04e35759f60c3de9c95e41d18f788a47c
Saif Mustaf <43801473+iamsaifmustaf@users.noreply.github.com>
2021-11-09 10:32:55 -0500

    Day6 Update

---
+++ Day6/reeborg_world_escaping_the_maze.py
@@ -0,0 +1,18 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+    
+# while not at_goal():
+    
+#     if at_goal():
+#         object_here()
+        
+#     while right_is_clear():
+#         turn_right()
+        
+#     if front_is_clear():
+#         move()
+#     else:
+#         turn_left()
---
+++ Day6/reeborg_world_hurdle_4.py
@@ -0,0 +1,24 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+    
+# while not at_goal():
+
+#     if at_goal():
+#         object_here()
+
+#     if front_is_clear():
+
+#         if right_is_clear():
+#             turn_right()
+#         else:
+#             move()
+
+#     if not front_is_clear():
+
+#         if right_is_clear():
+#             turn_right()
+#         else:
+#             turn_left()
