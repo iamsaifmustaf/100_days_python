68fd2e5a1c5c162dd1c5efacfc2a46f93543ad56
Saif Mustaf <43801473+iamsaifmustaf@users.noreply.github.com>
2021-11-10 17:52:56 -0500

    Day7 Hangman Challenge Complete

--- Day5/adding_even_numbers.py
+++ Day5/adding_even_numbers.py
@@ -5,4 +5,3 @@ for number in range(1,101,2):
     final_number += number
 
 print(f"\n\nThis is what you get when you add all even numbers from 1 to 100\n\n")
-tprint(str(final_number))
---
+++ Day7/hangman.py
@@ -0,0 +1,62 @@
+import hangman_words
+import hangman_pics
+from art import *
+import random
+import time
+from os import system
+
+word = ""
+final_answer = []
+hangman_steps = 0
+
+def check_input(char):
+    if char in word:
+        count = 0
+        if char in final_answer:
+            print(
+                hangman_pics.pics[hangman_steps]
+                + "\n\nYou already used this character, Try Again!\n\n"
+            )
+            globals()["hangman_steps"] += 1
+        while count < len(final_answer):
+            if char == word[count]:
+                final_answer[count] = char
+                count += 1
+            else:
+                count += 1
+        print("".join(final_answer))
+
+    else:
+        print(hangman_pics.pics[hangman_steps] + "\n\nWrong Entry, Try Again!\n\n")
+        globals()["hangman_steps"] += 1
+
+
+tprint("Welcome   to   hangman  !")
+
+category = int(
+    input(
+        "\n\nPlease Choose Your Category. Enter (1) for Animals, (2) for Car Brands: \n\n"
+    )
+)
+
+
+if category == 1:
+    word += hangman_words.animals[random.randint(0, len(hangman_words.animals) - 1)]
+    final_answer = ["_"] * len(word)
+if category == 2:
+    word += hangman_words.car_brands[
+        random.randint(0, len(hangman_words.car_brands) - 1)
+    ]
+    final_answer = ["_"] * len(word)
+
+word = word.lower()
+
+system("cls")
+
+while hangman_steps < 7:
+    time.sleep(1.5)
+    system('cls')
+    character = input("Please enter your character of choice: ")
+    check_input(character)
+
+print(f"\n\nThe word is {word}. Game Over, Better Luck Next Time!\n\n")
