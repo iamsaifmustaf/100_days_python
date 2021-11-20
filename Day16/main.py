61d574dfa1eadf074b971a95bc166df69492fe87
Saif Mustaf <43801473+iamsaifmustaf@users.noreply.github.com>
2021-11-20 15:36:19 -0500

    Day16 - Update

--- Day16/main.py
+++ Day16/main.py
@@ -1,13 +1,24 @@
-from prettytable import PrettyTable
-x = PrettyTable()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-x.field_names = ["Pokemon Name", "Type"]
-x.add_rows(
-    [
-        ["Pikachu", "Electric"],
-        ["Squirtle", "Water"],
-        ["Charmander", "Fire"]
-    ]
-)
-x.align = 'r'
-print(x)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+coffee_maker.report()
+money_machine.report
