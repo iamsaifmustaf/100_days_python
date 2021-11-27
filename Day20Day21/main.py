ba22c225a23cd56e879de3ade471ffff9dd84964
Saif Mustaf <43801473+iamsaifmustaf@users.noreply.github.com>
2021-11-26 21:54:52 -0500

    Day19Day20 - Complete Snake Movement with Key Press, Add Food and Collision with Food. Add Scoreboard

--- Day20Day21/main.py
+++ Day20Day21/main.py
@@ -1,7 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
-
+from food import Food
+from scoreboard import Scoreboard
 # Create Screen and set bg color, size and title
 
 screen = Screen()
@@ -11,6 +12,9 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.move_up,"Up")
 screen.onkey(snake.move_down,"Down")
@@ -19,9 +23,14 @@ screen.onkey(snake.move_right,"Right")
 
 
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    #Detect collision with Food
+    if snake.head.distance(food) < 15:
+        food.new_food_location()
+        scoreboard.increase_score()
 
 screen.exitonclick()
--- Day20Day21/snake.py
+++ Day20Day21/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 
@@ -10,6 +14,7 @@ class Snake:
     def __init__(self):
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
 
 
     def create_snake(self):
@@ -25,17 +30,24 @@ class Snake:
         for square_num in range(len(self.snake_body) - 1, 0 , -1):
             self.snake_body[square_num].goto(self.snake_body[square_num - 1].pos())
 
-        self.snake_body[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def move_right(self):
-        self.snake_body[0].setheading(0)
-        self.move()
+        if self.head.heading() == LEFT:
+            return()
+        self.head.setheading(RIGHT)
+    
     def move_up(self):
-        self.snake_body[0].setheading(90)
-        self.move()
+        if self.head.heading() == DOWN:
+            return()
+        self.head.setheading(UP)
+                
     def move_left(self):
-        self.snake_body[0].setheading(180)
-        self.move()
+        if self.head.heading() == RIGHT:
+            return()
+        self.head.setheading(LEFT)
+    
     def move_down(self):
-        self.snake_body[0].setheading(270)
-        self.move()
+        if self.head.heading() == UP:
+            return()
+        self.head.setheading(DOWN)
